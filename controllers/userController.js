298c1991dcdaca9853ab77c7d65f75f7847b1105
joshua <joshares9991@gmail.com>
2023-11-27 12:32:43 +0000

    cookie

--- controllers/userController.js
+++ controllers/userController.js
@@ -41,7 +41,7 @@ const createSendToken = (user, statusCode, res) => {
     httpOnly: true,
   };
 
-  // if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
+  if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
   res.cookie("jwt", token, cookieOptions);
   console.log(token);
 
